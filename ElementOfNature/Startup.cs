c0f366debf9f5265b51c7ed6fa59d53daf537632
Yaaroslaw <yaaroslaw@gmail.com>
2018-03-08 09:50:39 +0200

    Add project files.

---
+++ ElementOfNature/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ElementOfNature.Startup))]
+namespace ElementOfNature
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
